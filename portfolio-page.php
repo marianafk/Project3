1326fdc20008e292d10fcde4adc5650ccb71a921
marianafk <mfloresk@u.rochester.edu>
2016-12-19 20:43:50 -0500

    added portfolio page with base code

---
+++ portfolio-page.php
@@ -0,0 +1,65 @@
+<?php 
+  $currentPage = "Portfolio";
+  include "inc/top.inc"; 
+?>
+
+<?php 
+  $currentPage = "portfolio";
+  include "inc/nav.inc"; 
+?>
+
+<div class="porfolio">
+	<h2>Portfolio</h2>
+
+	<div id="one">
+		<img src="images/one.jpg" alt="One">
+		<div class="info">
+			<h3>Number One</h3>
+			<p>Description...</p>
+		</div>div>
+	</div>
+
+	<div id="two">
+		<img src="images/two.jpg" alt="One">
+		<div class="info">
+			<h3>Number Two</h3>
+			<p>Description...</p>
+		</div>div>
+	</div>
+
+	<div id="three">
+		<img src="images/three.jpg" alt="One">
+		<div class="info">
+			<h3>Number Three</h3>
+			<p>Description...</p>
+		</div>div>
+	</div>
+
+	<div id="four">
+		<img src="images/four.jpg" alt="One">
+		<div class="info">
+			<h3>Number Four</h3>
+			<p>Description...</p>
+		</div>div>
+	</div>
+
+	<div id="five">
+		<img src="images/five.jpg" alt="One">
+		<div class="info">
+			<h3>Number Five</h3>
+			<p>Description...</p>
+		</div>div>
+	</div>
+
+	<div id="six">
+		<img src="images/six.jpg" alt="One">
+		<div class="info">
+			<h3>Number Six</h3>
+			<p>Description...</p>
+		</div>div>
+	</div>
+
+
+<?php
+	include "inc/bottom.inc";
+?>
