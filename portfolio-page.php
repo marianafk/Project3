7347d4387751bed176d457c880feeecfa18e489e
marianafk <mfloresk@u.rochester.edu>
2016-12-19 23:33:41 -0500

    Added files for portfolio
    Added css files and other to make the portfolio plugin work. I added
    enough elements to make it look like my page, but I still left the
    entire structure as it was, since I was not very familiar with the
    entire code.

--- portfolio-page.php
+++ portfolio-page.php
@@ -8,55 +8,77 @@
   include "inc/nav.inc"; 
 ?>
 
-<div class="porfolio">
+<div class="portfolio">
 	<h2>Portfolio</h2>
 
 	<div id="one">
 		<img src="images/one.jpg" alt="One">
 		<div class="info">
-			<h3>Number One</h3>
-			<p>Description...</p>
-		</div>div>
+			<h3>Wordpress Website</h3>
+			<h4>Description:</h4>
+			<p>This was our lab 11. The purpose of this lab was to practice installing and using a very well known Content Management System. We had to be able to install it in our local machine using MAMP/WAMP, and then we had to move it to a production web server. To be able to see how our website looked, we had to add some random posts, comments, etc. We also had to add widgets and different elements to make it more interesting. I really enjoyed adding extra contect to my page, like the pictures that are displayed in the carousel.</p>
+			<h4>Link:</h4>
+			<a href="www.urcsc174.org/mfloresk/lab11">Lab 11</a>
+		</div>
 	</div>
 
 	<div id="two">
-		<img src="images/two.jpg" alt="One">
+		<img src="images/two.jpg" alt="Two">
 		<div class="info">
-			<h3>Number Two</h3>
-			<p>Description...</p>
-		</div>div>
+			<h3>Using a framework</h3>
+			<h4>Description:</h4>
+			<p>For this lab, we had to use some starter files and change the entire CSS code to be able to display the information in a completely different way. We had to understand the framework we wanted to use to be able to apply the necessary CSS and design the website in the way we wanted to. I really enjoyed working on this assignment, mainly because I got to see a different design for that website that we have used too many times in this course and in 170 :). I really liked the design I came up with, it was very clean.</p>
+			<h4>Link:</h4>
+			<a href="www.urcsc174.org/mfloresk/lab03">Lab 3</a>
+		</div>
 	</div>
 
+	<br>
+
 	<div id="three">
-		<img src="images/three.jpg" alt="One">
+		<img src="images/three.jpg" alt="Three">
 		<div class="info">
-			<h3>Number Three</h3>
-			<p>Description...</p>
-		</div>div>
+			<h3>Scrolly Website</h3>
+			<h4>Description:</h4>
+			<p>For this assignment, we had to pair up and design a very long scrolly website with 3 different sections. The sections had to demonstrate different pattern of design to show information in very different ways. I thought this project was very challenging, not because of having to work with another person, but because of the patterns we had to follow. It was hard to apply the necessary CSS to be able to show the information the way we wanted to. I think the easiest part was to design the section that contained our information.</p>
+			<h4>Link:</h4>
+			<a href="www.urcsc174.org/mfloresk/lab09">Lab 9</a>
+		</div>
 	</div>
 
 	<div id="four">
-		<img src="images/four.jpg" alt="One">
+		<img src="images/four.jpg" alt="Four">
 		<div class="info">
-			<h3>Number Four</h3>
-			<p>Description...</p>
-		</div>div>
+			<h3>First Lab</h3>
+			<h4>Description:</h4>
+			<p>I had so much fun working on this assignment. I thought it was great to be able to put any content I wanted to and give it a weird and different design. The thing I like about this lab is that we worked on it at the beginning of the semester, I can't believe it's that long since I had to design it. I love hedgehogs as you can see by the picture, it was fun to find cute pictures of them. Another thing I liked about this lab was that the design didn't have to be very complex, we could make it our own in very simple ways.</p>
+			<h4>Link:</h4>
+			<a href="www.urcsc174.org/mfloresk/lab01">Lab 1</a>
+		</div>
 	</div>
 
+	<br>
+
 	<div id="five">
-		<img src="images/five.jpg" alt="One">
+		<img src="images/five.jpg" alt="Five">
 		<div class="info">
-			<h3>Number Five</h3>
-			<p>Description...</p>
-		</div>div>
+			<h3>New York City</h3>
+			<h4>Description:</h4>
+			<p>This assignment was a great challenge. We had to look at a webiste, get the starter files and apply the necessary CSS to be able to replicate what we could see in a picture. We had to make sure everything looked the same way, the background, the columns, the table at the bottom. It was so much fun! The only thing I didn't like was that I became too perfectionist at some point, and I wanted to make sure every single pixel in my website looked as the one in the picture, I probably spent more than 5 hours working on all the details.</p>
+			<h4>Link:</h4>
+			<a href="www.urcsc174.org/mfloresk/lab06">Lab 6</a>
+		</div>
 	</div>
 
 	<div id="six">
-		<img src="images/six.jpg" alt="One">
+		<img src="images/six.jpg" alt="Six">
 		<div class="info">
-			<h3>Number Six</h3>
-			<p>Description...</p>
-		</div>div>
+			<h3>Fixing the code</h3>
+			<h4>Description:</h4>
+			<p>For this lab, we had to work on starter files provided by the professor. We had to use php includes to be able to separate file that was being repeated too many times across the website, like the navigation bar, or the footer. We also had to fix all the links that were included on the file so that they would redirect the user to the correct page, or at least so that they wouldn't get an error. The last thing we had to do was find a way to show the user the current page, in other words, the page that they were visiting in the nav bar.</p>
+			<h4>Link:</h4>
+			<a href="www.urcsc174.org/mfloresk/lab02">Lab 2</a>
+		</div>
 	</div>
 
 
---
+++ portfolio-page1.php
@@ -0,0 +1,171 @@
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src="js/modernizr.js"></script>
+	<link rel="stylesheet" href="css/reset.css"> <!-- CSS reset -->
+	<link rel="stylesheet" href="css/style.css"> <!-- Resource style -->
+	<link href="https://fonts.googleapis.com/css?family=Lora:400,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,400i,700,700i|Lato:400,400i,700,700i|Open+Sans:400,400i,700,700i" rel="stylesheet">
+	<link rel="stylesheet" href="css/fonts.css">
+  	
+	<title>Project 3 | Portfolio</title>
+</head>
+<body>
+<div class = container>
+		<nav class = "menu-bar">
+    		<ul>
+     			<li><a href="index.php">Home</a></li>
+     			<li class="active"><a href="portfolio-page1.php">Portfolio</a></li>
+     			<li><a href="contact-form.php">Contact Form</a></li>
+    		</ul>
+  		</nav>
+<div class="cd-3d-portfolio">
+	<nav class="cd-3d-portfolio-navigation">
+		<div class="cd-wrapper">
+  			<br>
+			<h1>Portfolio</h1>
+				
+			<ul>
+				<li><a href="#0" class="selected">One</a></li>
+				<li><a href="#0">Two</a></li>
+			</ul>
+		</div>
+	</nav> <!-- .cd-3d-portfolio-navigation -->
+	
+	<div class="projects">
+		<ul class="row">
+			<li class="front-face selected project-1">
+				<div class="project-wrapper">
+					<div class="project-image">
+						<div class="project-title">
+							<h2>Wordpress Website</h2>
+						</div>
+					</div> <!-- .project-image -->
+
+					<div class="project-content">
+						<div>
+							<p>This was our lab 11. The purpose of this lab was to practice installing and using a very well known Content Management System. We had to be able to install it in our local machine using MAMP/WAMP, and then we had to move it to a production web server. To be able to see how our website looked, we had to add some random posts, comments, etc. We also had to add widgets and different elements to make it more interesting. I really enjoyed adding extra contect to my page, like the pictures that are displayed in the carousel. Click on this link to see it ----> <a href="http://www.urcsc174.org/mfloresk/lab11">Lab 11</a></p>
+							
+						</div>
+					</div> <!-- .project-content -->
+
+					<a href="#0" class="close-project">Close</a>
+				</div> <!-- .project-wrapper -->
+			</li>
+
+			<li class="right-face project-2">
+				<div class="project-wrapper">
+					<div class="project-image">
+						<div class="project-title">
+							<h2>First Lab</h2>
+						</div>
+					</div> <!-- .project-image -->
+
+					<div class="project-content">
+						<div>
+							<p>I had so much fun working on this assignment. I thought it was great to be able to put any content I wanted to and give it a weird and different design. The thing I like about this lab is that we worked on it at the beginning of the semester, I can't believe it's that long since I had to design it. I love hedgehogs as you can see by the picture, it was fun to find cute pictures of them. Another thing I liked about this lab was that the design didn't have to be very complex, we could make it our own in very simple ways. Click on this link to see it ----><a href="http://www.urcsc174.org/mfloresk/lab01">Lab 1</a></p>
+						</div>
+					</div> <!-- .project-content -->
+
+					<a href="#0" class="close-project">Close</a>
+				</div> <!-- .project-wrapper -->
+			</li>
+		</ul> <!-- .row -->
+	
+		<ul class="row">
+			<li class="front-face selected project-4">
+				<div class="project-wrapper">
+					<div class="project-image">
+						<div class="project-title">
+							<h2>Using a Framework</h2>
+						</div>
+					</div> <!-- .project-image -->
+
+					<div class="project-content">
+						<div>
+							<p>For this lab, we had to use some starter files and change the entire CSS code to be able to display the information in a completely different way. We had to understand the framework we wanted to use to be able to apply the necessary CSS and design the website in the way we wanted to. I really enjoyed working on this assignment, mainly because I got to see a different design for that website that we have used too many times in this course and in 170 :). I really liked the design I came up with, it was very clean. Click on this link to see it ----> <a href="http://www.urcsc174.org/mfloresk/lab03">Lab 3</a></p>
+						</div>
+					</div> <!-- .project-content -->
+
+					<a href="#0" class="close-project">Close</a>
+				</div> <!-- .project-wrapper -->
+			</li>
+
+			<li class="right-face project-5">
+				<div class="project-wrapper">
+					<div class="project-image">
+						<div class="project-title">
+							<h2>New York City</h2>
+						</div>
+					</div> <!-- .project-image -->
+
+					<div class="project-content">
+						<div>
+							<p>This assignment was a great challenge. We had to look at a webiste, get the starter files and apply the necessary CSS to be able to replicate what we could see in a picture. We had to make sure everything looked the same way, the background, the columns, the table at the bottom. It was so much fun! The only thing I didn't like was that I became too perfectionist at some point, and I wanted to make sure every single pixel in my website looked as the one in the picture, I probably spent more than 5 hours working on all the details. Click on this link to see it ----><a href="http://www.urcsc174.org/mfloresk/lab06">Lab 6</a></p>
+						</div>
+					</div> <!-- .project-content -->
+
+					<a href="#0" class="close-project">Close</a>
+				</div> <!-- .project-wrapper -->
+			</li>
+		</ul> <!-- .row -->
+	
+		<ul class="row">
+			<li class="front-face selected project-7">
+				<div class="project-wrapper">
+					<div class="project-image">
+						<div class="project-title">
+							<h2>Scrolly Website</h2>
+						</div>
+					</div> <!-- .project-image -->
+
+					<div class="project-content">
+						<div>
+							<p>For this assignment, we had to pair up and design a very long scrolly website with 3 different sections. The sections had to demonstrate different pattern of design to show information in very different ways. I thought this project was very challenging, not because of having to work with another person, but because of the patterns we had to follow. It was hard to apply the necessary CSS to be able to show the information the way we wanted to. I think the easiest part was to design the section that contained our information. Click on this link to see it ----><a href="http://www.urcsc174.org/mfloresk/lab09">Lab 9</a></p>
+						</div>
+					</div> <!-- .project-content -->
+
+					<a href="#0" class="close-project">Close</a>
+				</div> <!-- .project-wrapper -->
+			</li>
+
+			<li class="right-face project-8">
+				<div class="project-wrapper">
+					<div class="project-image">
+						<div class="project-title">
+							<h2>Fixing the Code</h2>
+						</div>
+					</div> <!-- .project-image -->
+
+					<div class="project-content">
+						<div>
+							<p>For this lab, we had to work on starter files provided by the professor. We had to use php includes to be able to separate file that was being repeated too many times across the website, like the navigation bar, or the footer. We also had to fix all the links that were included on the file so that they would redirect the user to the correct page, or at least so that they wouldn't get an error. The last thing we had to do was find a way to show the user the current page, in other words, the page that they were visiting in the nav bar. Click on this link to see it ----> <a href="http://www.urcsc174.org/mfloresk/lab02">Lab 2</a></p>
+						</div>
+					</div> <!-- .project-content -->
+
+					<a href="#0" class="close-project">Close</a>
+				</div> <!-- .project-wrapper -->
+			</li>
+
+		</ul> <!-- .row -->
+	</div><!-- .projects -->
+
+	<a href="#0" class="cd-top">Top</a>
+</div>
+	
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+<script>
+	if( !window.jQuery ) document.write('<script src="js/jquery-3.0.0.min.js"><\/script>');
+</script>
+<script src="js/main.js"></script> <!-- Resource jQuery -->
+<footer>
+	<br>
+	<p>Mariana Flores Kim - Project 3</p>
+	<br>
+	<p>CSC174 Advanced Front End Web Development</p>
+</footer>
+</div>
+</body>
+</html>
